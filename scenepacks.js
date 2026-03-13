6045466cf0b33d350260549c231f9827aebcd3af
Coen-yin <107782@sje.vic.edu.au>
2026-03-13 23:12:54 +1100

    Update scenepacks.js

--- scenepacks.js
+++ scenepacks.js
@@ -61,10 +61,10 @@ const scenepacks = [
     download: "https://mega.nz"
   },
   {
-    title: "Zendaya Scenepack",
+    title: "Tom Holland",
     category: "celebrities",
-    image: "https://image.tmdb.org/t/p/w780/tyNOAiBs5NMcMCRpjwbEkFqdOqv.jpg",
-    download: "https://mega.nz"
+    image: "https://i.guim.co.uk/img/media/dd3acbcaaf6da1b900426ab7ca78bdab3338bf0b/333_91_2292_1376/master/2292.jpg?width=465&dpr=1&s=none&crop=none",
+    download: "https://mega.nz/file/XqIXgIBb#QcOHTU7wM4M21FyJDOi4r-Tyfbr9tv7Kmc70kPk2A0w"
   },
   {
     title: "Timothée Chalamet Scenepack",
